19f2340ed8d7c52b25d833b9682949bde8d389a8
Nicholas <atok948@gmail.com>
2024-05-20 13:50:12 +0000

    SetUp project

---
+++ script.js
@@ -0,0 +1,14 @@
+function applyRandomColors() {
+    const colors = ['green', 'blue', 'red'];
+    const h3Elements = document.querySelectorAll('h3');
+  
+    h3Elements.forEach(h3 => {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      h3.style.color = randomColor;
+    });
+  }
+  
+  document.querySelectorAll('h5').forEach(h5 => {
+    h5.addEventListener('click', applyRandomColors);
+  });
+  
